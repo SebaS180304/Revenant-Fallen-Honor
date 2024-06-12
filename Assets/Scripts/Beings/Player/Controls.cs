f98dbd76c36984cdb3b0003fcadb8eac1ac49fc1
SebaS180304 <drdiegoramos18@gmail.com>
2024-06-12 13:44:47 -0600

    pequeñas modificaciones

--- Assets/Scripts/Beings/Player/Controls.cs
+++ Assets/Scripts/Beings/Player/Controls.cs
@@ -61,6 +61,9 @@ public class Controls : MonoBehaviour
                 fire = firing ? true : fire;
 
             }
+            else {
+                xMov = 0f;
+            }
         }
     }
 
