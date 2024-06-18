1cf3af2e2bfcc37b06cc6a367d430a53cd1c59ba
SebaS180304 <drdiegoramos18@gmail.com>
2024-06-18 17:08:00 -0600

    yA QUEDO

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -20,13 +20,15 @@ public class Boss : MonoBehaviour
     public event EventHandler OnFire;
 
 
-    
+
     // Start is called before the first frame updat
 
     // Update is called once per frame
 
-    private void Awake() {
+    private void Awake()
+    {
         rightF = true;
+        player = GameObject.Find("Player");
         transform = GetComponent<Transform>();
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
@@ -35,66 +37,85 @@ public class Boss : MonoBehaviour
     }
     void Update()
     {
-        if(player == null){
+        if (player == null)
+        {
             animator.SetInteger("State", 2);
         }
 
         animator.SetFloat("VelY", rb2d.velocity.y);
     }
 
-    public GameObject getPlayer(){
+    public GameObject getPlayer()
+    {
         return player;
     }
-    public float getDist(){
-        return objective.position.x-transform.position.x;
+    public float getDist()
+    {
+        return objective.position.x - transform.position.x;
     }
-    public void setPlayer(GameObject ply){
+    public void setPlayer(GameObject ply)
+    {
         player = ply;
     }
 
-    public void Turn(){
-        if(rightF && (transform.position.x > objective.position.x)){
-            transform.Rotate(0f,180f,0f);
+    public void Turn()
+    {
+        if (rightF && (transform.position.x > objective.position.x))
+        {
+            transform.Rotate(0f, 180f, 0f);
             rightF = false;
         }
-        if(!rightF && (transform.position.x < objective.position.x)){
-            transform.Rotate(0f,180f,0f);
+        if (!rightF && (transform.position.x < objective.position.x))
+        {
+            transform.Rotate(0f, 180f, 0f);
             rightF = true;
         }
     }
-    public void ActuOption(){
-        if(player != null){
+    public void ActuOption()
+    {
+        if (player != null)
+        {
             objective = player.GetComponent<Transform>();
-            if(Math.Abs(getDist())> MAX_DIST ){
+            if (Math.Abs(getDist()) > MAX_DIST)
+            {
                 animator.SetInteger("State", -1);
-            }else{
+            }
+            else
+            {
                 animator.SetInteger("State", 1);
-                OnFire?.Invoke(this,EventArgs.Empty);
+
+
             }
         }
     }
 
-    public void CountDown(){
+    public void CountDown()
+    {
         StartCoroutine(DecicionCoolDown());
     }
-    private IEnumerator DecicionCoolDown(){
+    private IEnumerator DecicionCoolDown()
+    {
         yield return new WaitForSeconds(5f);
         ActuOption();
     }
 
-    public void EndFireCountDown(){
+    public void EndFireCountDown()
+    {
+        OnFire?.Invoke(this, EventArgs.Empty);
         StartCoroutine(FireCountDowwn());
     }
 
-    private IEnumerator FireCountDowwn(){
+    private IEnumerator FireCountDowwn()
+    {
         yield return new WaitForSeconds(3f);
         animator.SetBool("EndFire", true);
-        OnFire?.Invoke(this,EventArgs.Empty);
-    } 
-    public void Reset(){
+        OnFire?.Invoke(this, EventArgs.Empty);
+    }
+    public void Reset()
+    {
         player = null;
         transform.position = enemy.spawnpoint;
     }
-    
+
 
 }
